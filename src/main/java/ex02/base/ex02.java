f79879e1abea361af34fe1a06ef6556ad8ee7c72
rachelsch1 <schwarz.rachel.e@gmail.com>
2021-06-06 11:56:56 -0400

    initial solution

--- src/main/java/ex02/base/ex02.java
+++ src/main/java/ex02/base/ex02.java
@@ -1,4 +1,16 @@
 package ex02.base;
 
+import java.util.Scanner;
+
 public class ex02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String string = input.nextLine();
+        int length = string.length();
+
+        String outputString = string + " has " + length + " characters.";
+
+        System.out.println(outputString);
+    }
 }
